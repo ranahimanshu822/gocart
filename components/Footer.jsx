05cbe0b971c71ea9463ad85fe59a30a2f9c36347
Himanshu Rana <ranahimanshu822@gmail.com>
2026-04-18 16:28:51 +0530

    Initial commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
+                { text: "+9196907000", path: '/', icon: MailIcon },
                 { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "114 Roorkee, 247667", path: '/', icon: MapPinIcon }
             ]
         }
     ];
